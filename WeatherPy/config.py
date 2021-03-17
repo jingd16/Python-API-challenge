85f7f9883165e8748164585d05de9322d197c2fe
Jing Davies <jing.davies@hotmail.com>
2021-03-17 21:49:46 +1100

    Completed, report not done

---
+++ WeatherPy/config.py
@@ -0,0 +1,18 @@
+#OMDB
+api_key="91731ef6"
+
+#https://home.openweathermap.org/users/sign_in
+weather_map_key="8a16a1b305dccd76077ee68f5aef242d"
+
+#https://www.tvmaze.com/dashboard/index
+tv_key = "lATnda6-7VC6p_Z-SkFtNfl00LzxAAMO"
+
+#https://console.cloud.google.com/home/dashboard?folder=&organizationId=&project=skilful-nexus-307523
+google_key = "AIzaSyBvhQTctp4hXeOV-RVjhmkQYd1_37zr6N8"
+
+#http://www.census.gov/developers/
+census_key = "9b4819b4ffc086feafca809173107462ac3a0dc5"
+
+
+#https://developer.nytimes.com
+nyt_key = "QxhHJGqXym0VLk3TaG77ibPuC2CFJHGm"
